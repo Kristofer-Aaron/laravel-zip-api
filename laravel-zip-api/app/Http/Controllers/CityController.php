c4e5b425d67633cafea28d2bb57bcffce18c1fe6
Anook <44230962+Kristofer-Aaron@users.noreply.github.com>
2025-11-03 11:01:42 +0100

    API DocBlock comments

--- laravel-zip-api/app/Http/Controllers/CityController.php
+++ laravel-zip-api/app/Http/Controllers/CityController.php
@@ -9,13 +9,73 @@ use App\Models\County;
 
 class CityController extends Controller
 {
+    /**
+     * @api {get} /cities Get all cities
+     * @apiName GetCities
+     * @apiGroup City
+     *
+     * @apiSuccess {Object[]} cities List of cities.
+     * @apiSuccess {Number} cities.id City unique ID.
+     * @apiSuccess {String} cities.zip City ZIP code.
+     * @apiSuccess {String} cities.name City name.
+     * @apiSuccess {Object} cities.county Associated county.
+     * @apiSuccess {Number} cities.county.id County ID.
+     * @apiSuccess {String} cities.county.name County name.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *  HTTP/1.1 200 OK
+     *  [
+     *      {
+     *          "id": 1,
+     *          "zip": "1234",
+     *          "name": "Sample City",
+     *          "county": {
+     *              "id": 1,
+     *              "name": "Sample County"
+     *          }
+     *      }
+     *  ]
+     */
     public function index()
     {
         $cities = City::with('county')->get();
         return response()->json($cities, 200);
     }
 
-    
+    /**
+     * @api {get} /cities/:id Get city by ID
+     * @apiName GetCity
+     * @apiGroup City
+     *
+     * @apiParam {Number} id City unique ID.
+     *
+     * @apiSuccess {Number} id City unique ID.
+     * @apiSuccess {String} zip City ZIP code.
+     * @apiSuccess {String} name City name.
+     * @apiSuccess {Object} county Associated county.
+     * @apiSuccess {Number} county.id County ID.
+     * @apiSuccess {String} county.name County name.
+     *
+     * @apiError {String} message Error message.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *  HTTP/1.1 200 OK
+     *  {
+     *      "id": 1,
+     *      "zip": "1234",
+     *      "name": "Sample City",
+     *      "county": {
+     *          "id": 1,
+     *          "name": "Sample County"
+     *      }
+     *  }
+     *
+     * @apiErrorExample {json} Not Found:
+     *  HTTP/1.1 404 Not Found
+     *  {
+     *      "message": "City with id not found"
+     *  }
+     */
     public function show(int $id)
     {
         $city = City::with('county')->find($id);
@@ -27,7 +87,34 @@ class CityController extends Controller
         return response()->json($city, 200);
     }
 
-    
+    /**
+     * @api {post} /cities Create a new city
+     * @apiName CreateCity
+     * @apiGroup City
+     *
+     * @apiBody {String} zip City ZIP code (4 digits).
+     * @apiBody {String} name City name.
+     * @apiBody {String} county County name.
+     *
+     * @apiSuccess {Number} id City unique ID.
+     * @apiSuccess {String} zip City ZIP code.
+     * @apiSuccess {String} name City name.
+     * @apiSuccess {Object} county Associated county.
+     * @apiSuccess {Number} county.id County ID.
+     * @apiSuccess {String} county.name County name.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *  HTTP/1.1 201 Created
+     *  {
+     *      "id": 1,
+     *      "zip": "1234",
+     *      "name": "New City",
+     *      "county": {
+     *          "id": 1,
+     *          "name": "New County"
+     *      }
+     *  }
+     */
     public function store(Request $request)
     {
         $data = $request->validate([
@@ -47,7 +134,44 @@ class CityController extends Controller
         return response()->json($city->load('county'), 201);
     }
 
-
+    /**
+     * @api {put} /cities/:id Update city by ID
+     * @apiName UpdateCity
+     * @apiGroup City
+     *
+     * @apiParam {Number} id City unique ID.
+     *
+     * @apiBody {String} zip City ZIP code (4 digits).
+     * @apiBody {String} name City name.
+     * @apiBody {String} county County name.
+     *
+     * @apiSuccess {Number} id City unique ID.
+     * @apiSuccess {String} zip City ZIP code.
+     * @apiSuccess {String} name City name.
+     * @apiSuccess {Object} county Associated county.
+     * @apiSuccess {Number} county.id County ID.
+     * @apiSuccess {String} county.name County name.
+     *
+     * @apiError {String} message Error message.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *  HTTP/1.1 200 OK
+     *  {
+     *      "id": 1,
+     *      "zip": "1234",
+     *      "name": "Updated City",
+     *      "county": {
+     *          "id": 1,
+     *          "name": "Updated County"
+     *      }
+     *  }
+     *
+     * @apiErrorExample {json} Not Found:
+     *  HTTP/1.1 404 Not Found
+     *  {
+     *      "message": "City with id not found"
+     *  }
+     */
     public function update(Request $request, int $id)
     {
         $city = City::with('county')->find($id);
@@ -73,7 +197,24 @@ class CityController extends Controller
         return response()->json($city->load('county'), 200);
     }
 
-
+    /**
+     * @api {delete} /cities/:id Delete city by ID
+     * @apiName DeleteCity
+     * @apiGroup City
+     *
+     * @apiParam {Number} id City unique ID.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *  HTTP/1.1 204 No Content
+     *
+     * @apiError {String} message Error message.
+     *
+     * @apiErrorExample {json} Not Found:
+     *  HTTP/1.1 404 Not Found
+     *  {
+     *      "message": "City with id not found"
+     *  }
+     */
     public function destroy(int $id)
     {
         $city = City::with('county')->find($id);
--- laravel-zip-api/app/Http/Controllers/CountyController.php
+++ laravel-zip-api/app/Http/Controllers/CountyController.php
@@ -4,17 +4,59 @@ namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\City;
 use App\Models\County;
 
 class CountyController extends Controller
 {
+    /**
+     * @api {get} /counties Get all counties
+     * @apiName GetCounties
+     * @apiGroup County
+     *
+     * @apiSuccess {Object[]} counties List of counties.
+     * @apiSuccess {Number} counties.id County unique ID.
+     * @apiSuccess {String} counties.name County name.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *  HTTP/1.1 200 OK
+     *  [
+     *      {
+     *          "id": 1,
+     *          "name": "Sample County"
+     *      }
+     *  ]
+     */
     public function index()
     {
         $counties = County::get();
         return response()->json($counties, 200);
     }
 
+    /**
+     * @api {get} /counties/:id Get county by ID
+     * @apiName GetCounty
+     * @apiGroup County
+     *
+     * @apiParam {Number} id County unique ID.
+     *
+     * @apiSuccess {Number} id County unique ID.
+     * @apiSuccess {String} name County name.
+     *
+     * @apiError {String} message Error message.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *  HTTP/1.1 200 OK
+     *  {
+     *      "id": 1,
+     *      "name": "Sample County"
+     *  }
+     *
+     * @apiErrorExample {json} Not Found:
+     *  HTTP/1.1 404 Not Found
+     *  {
+     *      "message": "County with id not found"
+     *  }
+     */
     public function show(int $id)
     {
         $county = County::find($id);
@@ -26,7 +68,23 @@ class CountyController extends Controller
         return response()->json($county, 200);
     }
 
-
+    /**
+     * @api {post} /counties Create a new county
+     * @apiName CreateCounty
+     * @apiGroup County
+     *
+     * @apiBody {String} name County name (unique).
+     *
+     * @apiSuccess {Number} id County unique ID.
+     * @apiSuccess {String} name County name.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *  HTTP/1.1 201 Created
+     *  {
+     *      "id": 1,
+     *      "name": "New County"
+     *  }
+     */
     public function store(Request $request)
     {
         $data = $request->validate([
@@ -40,6 +98,33 @@ class CountyController extends Controller
         return response()->json($county, 201);
     }
 
+    /**
+     * @api {put} /counties/:id Update county by ID
+     * @apiName UpdateCounty
+     * @apiGroup County
+     *
+     * @apiParam {Number} id County unique ID.
+     *
+     * @apiBody {String} name County name.
+     *
+     * @apiSuccess {Number} id County unique ID.
+     * @apiSuccess {String} name County name.
+     *
+     * @apiError {String} message Error message.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *  HTTP/1.1 200 OK
+     *  {
+     *      "id": 1,
+     *      "name": "Updated County"
+     *  }
+     *
+     * @apiErrorExample {json} Not Found:
+     *  HTTP/1.1 404 Not Found
+     *  {
+     *      "message": "County with id not found"
+     *  }
+     */
     public function update(Request $request, int $id)
     {
         $county = County::find($id);
@@ -59,6 +144,24 @@ class CountyController extends Controller
         return response()->json($county, 200);
     }
 
+    /**
+     * @api {delete} /counties/:id Delete county by ID
+     * @apiName DeleteCounty
+     * @apiGroup County
+     *
+     * @apiParam {Number} id County unique ID.
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *  HTTP/1.1 204 No Content
+     *
+     * @apiError {String} message Error message.
+     *
+     * @apiErrorExample {json} Not Found:
+     *  HTTP/1.1 404 Not Found
+     *  {
+     *      "message": "County with id not found"
+     *  }
+     */
     public function destroy(int $id)
     {
         $county = County::find($id);
